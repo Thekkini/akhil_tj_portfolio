6bfbf378aa3e20a93904434eed9d0ee95b8b8342
AkshayBenny <akshaybennygh@gmail.com>
2022-01-29 00:01:58 +0530

    hero section completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='min-h-screen bg-myLight'>
+    <div className='min-h-screen bg-myLight px-[16px] lg:px-[160px]'>
       <Home />
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,20 @@
+//Data for social media links on hero section of home page
+export const socialMedia = [
+    {
+        name: 'Resume',
+        link: "/"
+    },
+    {
+        name: 'Figma',
+        link: "/"
+    },
+    {
+        name: 'Linkedin',
+        link: "/"
+    },
+    {
+        name: 'Twitter',
+        link: "/"
+    },
+       
+]
