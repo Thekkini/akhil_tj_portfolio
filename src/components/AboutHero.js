7524ccd7dede19e29374388e3fb2788da8a2b1c1
Akshay Benny <976Ab482a*123#>
2022-04-15 19:17:05 +0530

    update

--- src/components/AboutHero.js
+++ src/components/AboutHero.js
@@ -8,38 +8,40 @@ function AboutHero() {
   //   Aos.init({ duration: 1500, once: true });
   // }, []);
   return (
-    <div className='px-0 xl:px-[160px]  xl:pt-[80px]'>
-      <div className='grid-cols-2 xl:grid'>
+    <div className=' xl:px-[160px]  xl:pt-[80px]'>
+      <div className='flex justify-center'>
         <img
           className='aspect-square  w-full object-cover '
           src={aboutData.aboutMyself.img}
           alt='myPhoto'
         />
 
-        <div className='bg-myLight right-[20px] top-[250px] z-50 mb-[108px] px-[16px] pt-[24px] xl:relative  xl:mb-[400px] xl:max-w-[750px]'>
-          <h1 className='sub-heading text-[#000249]'>
-            Hi, I'm <span className='text-myRed'>Akhil T J</span>.
-          </h1>
-          <div>
-            <p className='sub-heading text-[#000249]'>
-              My peeps call me <span className='text-myRed'>T J</span> for
-              short.
+        <div className='bg-myLight  mb-[108px] px-[16px] pt-[24px] flex justify-center   xl:mb-[400px] xl:max-w-[750px]'>
+          <div className="">
+            <h1 className='sub-heading text-[#000249]'>
+              Hi, I'm <span className='text-myRed'>Akhil T J</span>.
+            </h1>
+            <div>
+              <p className='sub-heading text-[#000249]'>
+                My peeps call me <span className='text-myRed'>T J</span> for
+                short.
+              </p>
+            </div>
+            <p className='main-para text-[#000249] pt-[4px]'>
+              {aboutData.aboutMyself.philosophy}
+            </p>
+            <p className='para pt-[40px] xl:pt-[48px]'>
+              <div className='space-y-[48px] pb-[24px]'>
+                {aboutData.aboutMyself.bio.map((para, index) => {
+                  return (
+                    <div>
+                      <p className='text-[#000249]'>{para.para}</p>
+                    </div>
+                  );
+                })}
+              </div>
             </p>
           </div>
-          <p className='main-para text-[#000249] pt-[4px]'>
-            {aboutData.aboutMyself.philosophy}
-          </p>
-          <p className='para pt-[40px] xl:pt-[48px]'>
-            <div className='space-y-[48px] pb-[24px]'>
-              {aboutData.aboutMyself.bio.map((para, index) => {
-                return (
-                  <div>
-                    <p className="text-[#000249]">{para.para}</p>
-                  </div>
-                );
-              })}
-            </div>
-          </p>
         </div>
       </div>
     </div>
